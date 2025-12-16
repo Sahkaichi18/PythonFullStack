668361313d772c9118bae0f5a4ed55a20b7ef97b
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-16 18:45:10 +0530

    Quick update

--- Python/Basic_Python.py
+++ Python/Basic_Python.py
@@ -17,3 +17,46 @@ print(x+y)
 print(x-y)
 print(x*y)
 print(x/y)          
+
+#Assignment Operators
+
+a = 5
+b = 10
+
+a += b  # a = a + b
+print(a)
+
+a -=b
+print(a) #a = a - b
+
+a *= b
+print(a) # a = a * b        
+
+a /= b
+print(a) # a = a / b
+
+a %= b
+print(a) # a = a % b
+
+#comparison operator 
+
+p = 30
+q = 40
+
+print(p == q)  # equal to
+print(p != q)  # not equal to 
+print(p > q)   # greater than
+print(p < q)   # less than
+print(p >= q)  # greater than or equal to
+print(p <= q)  # less than or equal to
+
+#logical operators 
+
+m = True
+n = False
+
+print(m and n)  # logical AND
+print(m or n)   # logical OR        
+print(not m)    # logical NOT
+print(not n)    # logical NOT
+print(m and not n)  # logical AND with NOT
