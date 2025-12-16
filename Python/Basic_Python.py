74cb52f62c986e700679ae542b25bf39785bc655
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-16 19:52:28 +0530

    Quick update

--- Python/Basic_Python.py
+++ Python/Basic_Python.py
@@ -1,65 +1,65 @@
 # print("hello world")
 
-a =10
-print(a)
-print(a+5)
+# a =10
+# print(a)
+# print(a+5)
 
-b = "name"
-print(type(b))
+# b = "name"
+# print(type(b))
 
-#Operators 
+# #Operators 
 
-#Arithmetic Operators
+# #Arithmetic Operators
 
-x = 10
-y = 4
-print(x+y)
-print(x-y)
-print(x*y)
-print(x/y)          
+# x = 10
+# y = 4
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)          
 
-#Assignment Operators
+# #Assignment Operators
 
-a = 5
-b = 10
+# a = 5
+# b = 10
 
-a += b  # a = a + b
-print(a)
+# a += b  # a = a + b
+# print(a)
 
-a -=b
-print(a) #a = a - b
+# a -=b
+# print(a) #a = a - b
 
-a *= b
-print(a) # a = a * b        
+# a *= b
+# print(a) # a = a * b        
 
-a /= b
-print(a) # a = a / b
+# a /= b
+# print(a) # a = a / b
 
-a %= b
-print(a) # a = a % b
+# a %= b
+# print(a) # a = a % b
 
-#comparison operator 
+# #comparison operator 
 
-p = 30
-q = 40
+# p = 30
+# q = 40
 
-print(p == q)  # equal to
-print(p != q)  # not equal to 
-print(p > q)   # greater than
-print(p < q)   # less than
-print(p >= q)  # greater than or equal to
-print(p <= q)  # less than or equal to
+# print(p == q)  # equal to
+# print(p != q)  # not equal to 
+# print(p > q)   # greater than
+# print(p < q)   # less than
+# print(p >= q)  # greater than or equal to
+# print(p <= q)  # less than or equal to
 
-#logical operators 
+# #logical operators 
 
-m = True
-n = False
+# m = True
+# n = False
 
-print(m and n)  # logical AND
-print(m or n)   # logical OR        
-print(not m)    # logical NOT
-print(not n)    # logical NOT
-print(m and not n)  # logical AND with NOT
+# print(m and n)  # logical AND
+# print(m or n)   # logical OR        
+# print(not m)    # logical NOT
+# print(not n)    # logical NOT
+# print(m and not n)  # logical AND with NOT
 
 #decision making
 #1.conditional statements
@@ -67,8 +67,42 @@ print(m and not n)  # logical AND with NOT
 
 #if elif else example
 
-a = 100000000000
-b = 220
-c = 1900500000000000000000
+# a = 100000000000
+# b = 220
+# c = 1900500000000000000000
 
-if a > b and  a > c or a > b and a < c:
+# if a > b and  a > c or a:
+#     print("a is the largest")
+# elif b > a and b > c:
+#     print("b is the largest")
+# else:
+#     print("c is the largest")
+
+
+a = 10
+b = 10
+c = 10
+
+if a == b == c:
+    print("All the values are same. No second largest number.")
+
+elif (a >= b and a <= c) or (a <= b and a >= c):
+    print("The second largest number is", a)
+
+elif (b >= a and b <= c) or (b <= a and b >= c):
+    print("The second largest number is", b)
+
+else:
+    print("The second largest number is", c)
+
+
+
+
+
+    
+
+
+
+
+
+
