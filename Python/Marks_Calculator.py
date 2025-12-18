66505ac9c7f7b9dc518c7d3712a8f4b904789873
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-18 18:45:29 +0530

    Quick update

---
+++ Python/Marks_Calculator.py
@@ -0,0 +1,13 @@
+input =  (int(input("Enter marks obtained : ")))
+
+c = (int(input("Enter the marks for C : ")))
+java = (int(input("Enter the marks for Java : ")))
+python = (int(input("Enter the marks for Python : ")))
+dsa = (int(input("Enter the marks for DSA : ")))
+oops = (int(input("Enter the marks for OOPS : ")))
+
+
+
+
+
+
