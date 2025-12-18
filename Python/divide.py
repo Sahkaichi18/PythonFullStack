2fd3e2fa6fe7e0054d63ecf75734486befcbe90d
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-18 19:29:30 +0530

    Quick update

--- Python/divide.py
+++ Python/divide.py
@@ -1,7 +1,6 @@
-#if a number is divisible by 3 and 5 then successfull or else unsuccessfull
+num = int(input("Enter a number: "))
 
-num = (int(input("Enter a number")))
-if(num/3 and 5 == 0):
-    print("Successfull")
-else :
-    print("Unsuccesfull")
+if num % 3 == 0 and num % 5 == 0:
+    print("Successful")
+else:
+    print("Unsuccessful")
