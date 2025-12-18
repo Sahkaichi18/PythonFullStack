46506c9b6a250ddc8b8a4aa75342a7fab8170982
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-18 20:01:26 +0530

    Quick update

--- Python/leap_Yea.py
+++ Python/leap_Yea.py
@@ -1,10 +1,9 @@
-year = int(input("Enter the year: "))
-
-if year % 400 == 0:
-    print(year, "is a leap year")
-elif year % 100 == 0:
-    print(year, "is not a leap year")
-elif year % 4 == 0:
-    print(year, "is a leap year")
-else:
-    print(year, "is not a leap year")
+for year in range(2002, 2035):
+    if year % 400 == 0:
+        print(year, "is a leap year")
+    elif year % 100 == 0:
+        print(year, "is not a leap year")
+    elif year % 4 == 0:
+        print(year, "is a leap year")
+    else:
+        print(year, "is not a leap year")
