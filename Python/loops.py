276109650f35bbf9f1c3659adc5157955b7b9b06
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-18 19:50:59 +0530

    Quick update

--- Python/loops.py
+++ Python/loops.py
@@ -3,3 +3,15 @@ for i in range (1, 11):
 
 for i in range (1,11,2): #in (1, 11, 2) 1 is the start| 11 is the ending |and 2 is the skipping|
     print(i *10)
+
+for i in range (1,11):
+    print(i*2)
+
+for i in range (2,21):
+    for j in range(1,11):
+        print(i*j)
+
+
+
+
+
