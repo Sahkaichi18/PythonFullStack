2c39ed65da3b9c456560349946015fad6008c26f
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-18 19:56:09 +0530

    Quick update

--- Python/even_odd_100to500.py
+++ Python/even_odd_100to500.py
@@ -1,6 +1,6 @@
-num = int(input("Enter a number"))
-
-if(num % 2 == 0):
-    print("The even number :", num)
-else:
-    print("The odd numbers are :" num)
+num = int(input("Enter a number : "))
+for i in range(99,501):
+    if(num % 2 == 0):
+        print("The even number :", num)
+    else:
+        print("The odd numbers are :", num)
