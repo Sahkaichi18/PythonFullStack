9d53b9560485c41176b075cbd69c2fb076927ee4
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-21 11:58:13 +0530

    Quick update

--- Python/Question.py
+++ Python/Question.py
@@ -1,13 +1,6 @@
 # user input a number of 3 digit.....check if its an armstrong.
-# Make a hotel management having 5 products.....user selects it and let there be a choice of payment
 
-# num = (int(input("Enter a 3 digit number : ")))
-# if (num*num*num +num == num):
-#     print(num ,"is an armstrong")
-# else:
-#     print(num ,"is not an armstrong")
-
-num = int(input("Enter a 3-digit number: "))
+num = int(input("Enter a 3 digit number : "))
 
 if num >=100 and num <=999:
 
