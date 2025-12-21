5d4c3005de72a6aae37a8e77694eb2e390f903b6
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-21 11:57:35 +0530

    Quick update

--- Python/Question.py
+++ Python/Question.py
@@ -12,14 +12,14 @@ num = int(input("Enter a 3-digit number: "))
 if num >=100 and num <=999:
 
     original = num
-    sum_of_cubes = 0
+    sum = 0
 
     while num > 0:
         digit = num % 10
-        sum_of_cubes += digit * digit * digit   # instead of **
+        sum += digit * digit * digit   
         num //= 10
 
-    if sum_of_cubes == original:
+    if sum == original:
         print(original, "is an Armstrong number")
     else:
         print(original, "is not an Armstrong number")
