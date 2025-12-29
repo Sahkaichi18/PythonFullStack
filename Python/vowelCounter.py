a8899dddd2ebd59c93738eb58588ff54c152e1e0
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-29 19:50:38 +0530

    Quick update

--- Python/vowelCounter.py
+++ Python/vowelCounter.py
@@ -2,14 +2,16 @@ a = ["data", "science", "python", "java", "html"]
 
 print(a)
 
-vowel= 0
+vowel = 0
 consonant = 0
 
 for i in a:
-    for  j in i:
-        if j in "aeiouAEIOU" :
+    for j in i:
+        if j in "aeiouAEIOU":
             vowel += 1
-        print("Count of vowel is : ", vowel)
-    else:
-        consonant += 1
-        print("Count of consonant is : ", consonant)
+        else:
+            consonant += 1
+
+print("Count of vowel is :", vowel)
+print("Count of consonant is :", consonant)
+
