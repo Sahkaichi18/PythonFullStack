06ae19ece169ed6846bb8543e970af411791484f
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-29 18:51:41 +0530

    Quick update

--- Python/list.py
+++ Python/list.py
@@ -2,7 +2,13 @@
 
 # every value in square brackets has index
 
+# we can update the elements in list
+
 a = [1,2,3,4,5,6,7,8]
 print(a)
+print("-------------------------")
 a[4]= 33
 print(a)
+print("-------------------------")
+print (a[1:4])
+
