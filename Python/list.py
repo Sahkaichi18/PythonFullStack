a06571ad91fab5c9af88274310a648c97765f2cb
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-29 19:10:09 +0530

    Quick update

--- Python/list.py
+++ Python/list.py
@@ -4,11 +4,27 @@
 
 # we can update the elements in list
 
-a = [1,2,3,4,5,6,7,8]
-print(a)
-print("-------------------------")
-a[4]= 33
-print(a)
-print("-------------------------")
-print (a[1:4])
+# a = [1,2,3,4,5,6,7,8]
+# print(a)
+# print("-------------------------")
+# a[4]= 33
+# print(a)
+# print("-------------------------")
+# print (a[1:4])
 
+# for i in range(0, len(a)):
+#     print(a[i])
+
+# for i in a:
+#     print(a)
+
+b = [10, 20, 30, 40, 50]
+
+sum = 0
+# for i in range(len(b)):
+#     sum = sum + b[i]
+#     print(sum)
+
+for i in b:
+    sum+=i
+print("The sum is", sum)
