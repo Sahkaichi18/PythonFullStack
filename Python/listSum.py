2d5099897db00bd1681d110cd1d0acf0be20aef8
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-30 20:10:22 +0530

    Quick update

--- Python/listSum.py
+++ Python/listSum.py
@@ -1,15 +1,31 @@
+# a = [1539, 5579, 5545, 3173]
+
+# sum = 0
+# # for i in range(len(a)):
+# #         sum = sum + a[i]
+# #         print(sum)
+
+# for Bnum in a:
+#     for Snum in Bnum:
+#         sum = sum + a[Snum]
+
+#         while Bnum > 0:
+#             digit = Bnum % 10
+#             sum += a[Snum]
+#             num //= 10
+
+#             print(sum)
+
+
 a = [1539, 5579, 5545, 3173]
 
-sum = 0
-# for i in range(len(a)):
-#         sum = sum + a[i]
-#         print(sum)
-
 for Bnum in a:
-        while Bnum > 0:
-            for Snum in Bnum:
-                digit = Bnum % 10
-                sum += a[Snum]
-                num //= 10
+        temp = Bnum
+        sum = 0
 
-                print(sum)
+        while temp > 0:
+            digit = temp % 10
+            sum += digit
+            temp //= 10
+
+        print("Sum of digits of", Bnum, "is", sum)
