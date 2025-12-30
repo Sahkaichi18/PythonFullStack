c27439b12479043786fe1f47d6c3cf318e461711
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-30 19:17:18 +0530

    Quick update

--- Python/listLeapYear.py
+++ Python/listLeapYear.py
@@ -1,6 +1,13 @@
-a = [2014, 2003, 2004, 202, 2024, 2025]
+a = [2014, 2003, 2004, 2028, 2024, 2025]
 
 for year in a:
     if(year % 4 == 0):
         print(year, "is a leap year")
+    elif(year % 100 == 0):
+        print(year , "is not a leap year")
+    elif(year % 400 == 0):
+        print(year, "is a leap year")
+    else:
+        print(year, "is not a leap year")
+
    
