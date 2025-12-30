ca40d8e645343cdb00653343a31dc1f6020a7b8b
Sahkaichi18 <sahilbhosale1804@gmail.com>
2025-12-30 19:01:48 +0530

    Quick update

--- Python/listArmstrong.py
+++ Python/listArmstrong.py
@@ -1,18 +1,38 @@
 #Get the armstrong of the given list
 
-b = [159, 133, 393, 904, 204]
+# b = [153, 133, 393, 904, 204]
+
+# for num in b:
+#     sum = 0
+#     og = num
+#     while num > 0:
+#         digit = num % 10
+#         sum += digit * digit * digit
+#         num //= 10
+
+#         if sum == og:
+#             print(og, "is an Armstrong number")
+#     else:
+#         print(og, "is not an Armstrong number")
+
+
+
+
+b = [153, 133, 393, 904, 204]
 
 for num in b:
     sum = 0
     og = num
-    if num > 0:
-        digit = num % 10
-        sum += digit * digit * digit
-        num //= 10
+
+    if num >= 100 and num <= 999:
+        while num > 0:
+            digit = num % 10
+            sum += digit * digit * digit
+            num //= 10
 
         if sum == og:
             print(og, "is an Armstrong number")
+        else:
+            print(og, "is not an Armstrong number")
     else:
-        print(og, "is not an Armstrong number")
-else:
-    print(num ,"is not a 3 digit number")
+        print(og, "is not a 3-digit number")
