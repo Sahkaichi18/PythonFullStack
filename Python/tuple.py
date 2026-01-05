affeaa4dee56df520f7d8e2218042b7c83037bdb
Sahkaichi18 <sahilbhosale1804@gmail.com>
2026-01-05 18:50:36 +0530

    Quick update

--- Python/tuple.py
+++ Python/tuple.py
@@ -4,3 +4,15 @@ print(type(a))
 
 #item assignment not allowed means changing values not allowed 
 # a[1]=30
+
+a = 1,2,3,4,5
+sum = 0
+#to show addition of the 1st and 2nd number.
+
+for i in range(0,len(a)):
+    sum += a[i]
+    print(sum)
+# for i in a:
+#     sum += i
+#     print(sum)
+
