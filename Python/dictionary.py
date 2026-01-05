1fdfe1a3ee5bdc426c190bef8a9d8dee64e7a624
Sahkaichi18 <sahilbhosale1804@gmail.com>
2026-01-05 19:34:08 +0530

    Quick update

--- Python/dictionary.py
+++ Python/dictionary.py
@@ -1,15 +1,27 @@
 #set data cant change values and cant add duplicates
 #Sets are unordered in Python
 
-a = {30,20,10,40,100}
-print(a)
+# a = {30,20,10,40,100}
+# print(a)
 
 # DICTIONARY
 # can store duplicate values but not keys
 
-a = {1:10, 3:20, 9:100}
-sum = 0
-for i in a:
-    sum+=a[i]
-    print(sum)
+# a = {1:10, 3:20, 9:100}
+# sum = 0
+# for i in a:
+#     sum+=a[i]
+#     print(sum)
+
+#Reverse name in dictionary
+
+s = {1:'Sahil', 2:'Hritik', 3:'Rudra'}
+
+for key in s:
+    name = s[key]
+    rev = ""
+    for ch in name:
+        rev = ch + rev
+    print(rev)
+
 
