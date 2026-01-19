ae2345f4d3938a6079f5d6e1ae07d81c7195b0fd
Sahkaichi18 <sahilbhosale1804@gmail.com>
2026-01-19 19:48:53 +0530

    Quick update

--- Python/buildInFnc.py
+++ Python/buildInFnc.py
@@ -1,60 +1,60 @@
-# print("Case conversion") 
+print("Case conversion") 
 
-# s = 'hello world'
+s = 'hello world'
 
-# print(s.upper())
+print(s.upper())
 
-# print(s.lower())
+print(s.lower())
 
-# print(s.title())
+print(s.title())
 
-# print(s.capitalize())
+print(s.capitalize())
 
-# print("--------------------------------")
+print("--------------------------------")
 
-# print("Search and replace")
+print("Search and replace")
 
-# text = "Python is fun, and python is powerful."
+text = "Python is fun, and python is powerful."
 
-# print(text.index('Python'))
+print(text.index('Python'))
 
-# print(text.replace("Python", "Java"))
+print(text.replace("Python", "Java"))
 
-# print(text.count("Python"))
+print(text.count("Python"))
 
-# print("--------------------------------")
+print("--------------------------------")
 
-# print("Formatting")
+print("Formatting")
 
-# word = "Python"
+word = "Python"
 
-# print(word.strip())
+print(word.strip())
 
-# print(word.lstrip())
+print(word.lstrip())
 
-# print(word.rstrip())
+print(word.rstrip())
 
-# print(word.center(100,'-'))
+print(word.center(100,'-'))
 
-# print(word.ljust(100,'*'))
+print(word.ljust(100,'*'))
 
-# print(word.rjust(100,'*'))
+print(word.rjust(100,'*'))
 
-# print("--------------------------------")
+print("--------------------------------")
 
-# print("Validation")
+print("Validation")
 
-# print("Python123".isalnum())
+print("Python123".isalnum())
 
-# print("Hello".isalpha())
+print("Hello".isalpha())
 
-# print(" ".isspace())
+print(" ".isspace())
 
-# print("python".islower())
+print("python".islower())
 
-# print("PYTHON".isupper())
+print("PYTHON".isupper())
 
-# print("--------------------------------")
+print("--------------------------------")
 
 print("Spliting and joining")
 
@@ -66,6 +66,32 @@ print(data.rsplit(",",1))
 
 text = "Hello \nWorld"
 print(text.splitlines())
+
+words = ['i', 'love', 'python']
+print("".join(words))
+
+print("--------------------------------")
+
+print("String Checking")
+
+print("HelloWorld".startswith("Hello"))
+print("HellowWorld".endswith("World"))
+
+print("--------------------------------")
+
+print("Others")
+
+print(len("Python"))
+
+print(ord('A')) #Ascii values
+
+print(chr(65))
+
+print("My name is {0} and i am {1} years old {2}".format("Alice", 25,""))
+
+
+
+
 
 
 
