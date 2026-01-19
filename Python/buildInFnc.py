fbc06b75f889639ee0aa19b312df4872b59f6e05
Sahkaichi18 <sahilbhosale1804@gmail.com>
2026-01-19 19:30:49 +0530

    Quick update

--- Python/buildInFnc.py
+++ Python/buildInFnc.py
@@ -1,4 +1,77 @@
-#Case conversion 
+# print("Case conversion") 
 
-s = 'hello world'
-print(s.upper())
+# s = 'hello world'
+
+# print(s.upper())
+
+# print(s.lower())
+
+# print(s.title())
+
+# print(s.capitalize())
+
+# print("--------------------------------")
+
+# print("Search and replace")
+
+# text = "Python is fun, and python is powerful."
+
+# print(text.index('Python'))
+
+# print(text.replace("Python", "Java"))
+
+# print(text.count("Python"))
+
+# print("--------------------------------")
+
+# print("Formatting")
+
+# word = "Python"
+
+# print(word.strip())
+
+# print(word.lstrip())
+
+# print(word.rstrip())
+
+# print(word.center(100,'-'))
+
+# print(word.ljust(100,'*'))
+
+# print(word.rjust(100,'*'))
+
+# print("--------------------------------")
+
+# print("Validation")
+
+# print("Python123".isalnum())
+
+# print("Hello".isalpha())
+
+# print(" ".isspace())
+
+# print("python".islower())
+
+# print("PYTHON".isupper())
+
+# print("--------------------------------")
+
+print("Spliting and joining")
+
+data = "apple,banana,grape"
+
+print(data.split(","))
+
+print(data.rsplit(",",1))
+
+text = "Hello \nWorld"
+print(text.splitlines())
+
+
+
+
+
+
+
+
+
